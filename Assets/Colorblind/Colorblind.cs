3fc58448b54447fb00e13050a7f2835f0f309ea3
Ian Eldred Pudney <puddles@google.com>
2022-06-28 23:51:06 +0200

    Add Apache license

--- Assets/Colorblind/Colorblind.cs
+++ Assets/Colorblind/Colorblind.cs
@@ -1,4 +1,20 @@
-﻿/** Color Blindness Simulator.
+﻿/*
+   Copyright 2016 Ian Eldred Pudney
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/** Color Blindness Simulator.
   * 
   * This script (and attached shader) can be used to simulate color blindness in Unity. 
   * To use, attach the script to a camera. A drop-down in the inspector can be used to
